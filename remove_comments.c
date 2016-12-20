a5acb49daf00395f23d64c35a0b39e3d271347ed
Anne <anne.cognet@holbertonschool.com>
2016-12-20 19:10:48 +0000

    added stripping of comments (#)

---
+++ remove_comments.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+/**
+ * remove_comments - remove everything after #
+ * @line: input line
+ * Return: a clean line
+ */
+char *remove_comments(char *line)
+{
+	char **split;
+	char *good;
+	int i;
+
+	split = strtow(line, '#');
+	if (split == NULL)
+		return (NULL);
+
+	good = split[0];
+	printf("%s the good stuff %s\n", __FILE__, good);
+	i = 1;
+	while (split[i] != NULL)
+		++i;
+	while (--i >= 1)
+		free(split[i]);
+	free(split);
+	return (good);
+}
