4a8ca06fa972d2a53a12286c763e49bfb581af7c
Jianqin Wang <jianqinwang94@gmail.com>
2016-12-20 20:02:08 -0800

    include flags for exit function

--- _exit_.c
+++ _exit_.c
@@ -10,18 +10,24 @@
  */
 int _exit_(char **enva, node_t **pathl, node_t **histl, char **args)
 {
+	int i;
+
+	i = 0;
 	printf("going to write out history\n");
 	_history_write(histl);
 	printf("GOING TO FREE enva\n");
 	free_strtow(enva);
-
 	free_list(*pathl);
 	free_list(*histl);
 	if (args != NULL)
 	{
+		if (args[1] != NULL)
+		{
+			i = _atoi(args[1]);
+		}
 		printf("%s FREE ARGS\n", __FILE__);
 		free_strtow(args);
 	}
-	exit(0);
+	exit(i);
 	return (0);
 }
